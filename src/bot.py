30017ea6483ab4fdc03c880d65513af09e76f811
Artyom Kharkovsky <kharkovsky@protonmail.com>
2018-01-25 02:58:04 +0300

    Wrapped with decorators

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
+import time
 import sys  # For passing -t or --token right from terminal
 import logging  # Only service information without any user data
 import os
+from functools import wraps
 
-import time
 from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from telegram.error import (TelegramError, Unauthorized, BadRequest,
                             TimedOut, ChatMigrated, NetworkError)
-
 
 from users import Users, Debug
 
@@ -16,9 +16,44 @@ from users import Users, Debug
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+ADMINS = [354454314]
 APP_FOLDER = os.path.dirname(os.path.realpath(__file__))
+STATE_STARTED, STATE_NOT_STARTED = range(2)
 
 users = None
+
+
+def if_admin(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in ADMINS:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
+
+def if_started(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        you = update.effective_user.id
+        if not users.exists(you):
+            bot.send_message(chat_id=you, text="*PRO TIP*: type /start to start",
+                             parse_mode=ParseMode.MARKDOWN)
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
+
+def if_chatting(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        you = update.effective_user.id
+        if not users.is_chatting(you):
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
 
 
 def init():
@@ -41,14 +76,9 @@ def command_start(bot, update):
                          parse_mode=ParseMode.MARKDOWN)
 
 
+@if_started
 def command_search(bot, update):
     you = update.message.chat_id
-
-    if not users.exists(you):
-        bot.send_message(chat_id=update.message.chat_id,
-                         text="*PRO TIP*: type /start to start",
-                         parse_mode=ParseMode.MARKDOWN)
-        return
 
     if users.in_search(you):
         return
@@ -65,21 +95,37 @@ def command_search(bot, update):
         users.search_add(you)
 
 
+@if_started
+@if_chatting
 def messages(bot, update):
     you = update.message.chat_id
+    target = users.get(you).target.id
 
-    if not users.exists(you):
-        bot.send_message(chat_id=you,
-                         text="*PRO TIP*: type /start to start",
-                         parse_mode=ParseMode.MARKDOWN)
-        return
-
-    if users.is_chatting(you):
-        bot.send_message(chat_id=users.get(you).target.id,
-                         text="*Stranger*\n{}".format(update.message.text),
-                         parse_mode=ParseMode.MARKDOWN)
+    bot.send_message(chat_id=target,
+                     text="*Stranger*\n{}".format(update.message.text),
+                     parse_mode=ParseMode.MARKDOWN)
 
 
+@if_started
+@if_chatting
+def messages_photo(bot, update):
+    you = update.message.chat_id
+    target = users.get(you).target.id
+    caption = update.message.caption
+
+    if caption is not None:
+        caption_title = "Stranger: "
+        if len(caption) <= 200-len(caption_title):  # 200 - Max recommended caption len
+            caption = "{}{}".format(caption_title, caption)
+        else:
+            bot.send_message(chat_id=target, text="Photo from Stranger:")
+
+    bot.send_photo(chat_id=target,
+                   photo=update.message.photo.pop().file_id,
+                   caption=caption)
+
+
+@if_started
 def command_bye(bot, update):
     you = update.message.chat_id
 
@@ -97,34 +143,24 @@ def command_bye(bot, update):
                              parse_mode=ParseMode.MARKDOWN)
 
 
+@if_started
 def command_settings(bot, update):
     you = update.message.chat_id
 
-    if not users.exists(you):
-        bot.send_message(chat_id=you,
-                         text="*PRO TIP*: type /start to start",
-                         parse_mode=ParseMode.MARKDOWN)
-        return
+    bot.send_message(chat_id=you, text="TODO: settings")
 
 
+@if_started
 def command_offer(bot, update):
     you = update.message.chat_id
-
-    if not users.exists(you):
-        bot.send_message(chat_id=you,
-                         text="*PRO TIP*: type /start to start",
-                         parse_mode=ParseMode.MARKDOWN)
-    elif users.is_chatting(you):
+    if users.is_chatting(you):
         update.message.reply_text("TODO: offer")
 
 
+@if_started
 def command_cancel(bot, update):
     you = update.message.chat_id
-    if not users.exists(you):
-        bot.send_message(chat_id=you,
-                         text="*PRO TIP*: type /start to start",
-                         parse_mode=ParseMode.MARKDOWN)
-    elif users.in_search(you):
+    if users.in_search(you):
         users.search_remove(you)
         bot.send_message(chat_id=you,
                          text="*Canceled.*\n/search - Search for chat",
@@ -132,14 +168,9 @@ def command_cancel(bot, update):
                          parse_mode=ParseMode.MARKDOWN)
 
 
+@if_started
 def command_stop(bot, update):
     you = update.message.chat_id
-
-    if not users.exists(you):
-        bot.send_message(chat_id=update.message.chat_id,
-                         text="*PRO TIP*: type /start to start",
-                         parse_mode=ParseMode.MARKDOWN)
-        return
 
     if users.is_chatting(you):
         command_bye(bot, update)
@@ -147,11 +178,18 @@ def command_stop(bot, update):
         users.search_remove(you)
 
     users.remove(you)
-
     update.message.reply_text("Bye!")
+    return STATE_NOT_STARTED
 
 
-def command_print(bot, update):
+@if_started
+def command_unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Unrecognized command :( Need /help?")
+
+
+@if_started
+@if_admin
+def command_debug(bot, update):
     you = update.message.chat_id
     commands = set(update.message.text.split(' ')[1:])
 
@@ -207,7 +245,7 @@ def main():
     dp = upd.dispatcher
 
     dp.add_handler(CommandHandler('start', command_start))
-    dp.add_handler(CommandHandler('print', command_print))  # DEBUG: print objects
+    dp.add_handler(CommandHandler('debug', command_debug))  # DEBUG: print objects
     dp.add_handler(CommandHandler('search', command_search))
     dp.add_handler(CommandHandler('bye', command_bye))
     dp.add_handler(CommandHandler('offer', command_offer))
@@ -215,10 +253,12 @@ def main():
     dp.add_handler(CommandHandler('stop', command_stop))
     dp.add_handler(CommandHandler('cancel', command_cancel))
     dp.add_handler(MessageHandler(Filters.text, messages))
+    dp.add_handler(MessageHandler(Filters.command, command_unknown))
+    dp.add_handler(MessageHandler(Filters.photo, messages_photo))
     dp.add_error_handler(error_handler)
 
     print("Bot @{} has been started!".format(upd.bot.username))
-    upd.start_polling()
+    upd.start_polling(poll_interval=.3)
     upd.idle()
 
     del users
